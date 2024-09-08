35e38478b950cb95484df5faf0ff702095653f6d
Artem <118220003+guntersteam@users.noreply.github.com>
2024-09-08 20:25:38 +0300

    feat: added repositories

---
+++ server/RateMe.API/RateMe.Persistence/Repositories/PlayListRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RateMe.Core.Abstractions;
+using RateMe.Persistence.Entities;
+
+namespace RateMe.Persistence.Repositories;
+
+public class PlayListRepository : IRepository<PlayListEntity>
+{
+   private readonly RateMeDbContext _context;
+
+   public PlayListRepository(RateMeDbContext context)
+   {
+      _context = context;
+   }
+
+   public async Task<Guid> Create(PlayListEntity entity)
+   {
+      var playListEntity = new PlayListEntity
+      {
+         PlayListId = entity.PlayListId,
+         UserId = entity.UserId,
+         PlayListName = entity.PlayListName,
+         ArtistName = entity.ArtistName,
+      };
+
+      await _context.AddAsync(playListEntity);
+      await _context.SaveChangesAsync();
+
+      return playListEntity.PlayListId;
+   }
+   
+
+   public async Task<List<PlayListEntity?>> Get()
+   {
+      var playlists = await _context.PlayLists.AsNoTracking().ToListAsync();
+
+      return playlists;
+   }
+
+   public async Task<Guid> Delete(Guid id)
+   {
+      await _context.PlayLists
+         .Where(p => p.PlayListId == id)
+         .ExecuteDeleteAsync();
+
+      return id;
+   }
+
+   public async Task<PlayListEntity?> GetById(Guid id)
+   {
+      return await _context.PlayLists
+         .AsNoTracking()
+         .FirstOrDefaultAsync(p => p.PlayListId == id);
+   }
+}
---
+++ server/RateMe.API/RateMe.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RateMe.Core.Abstractions;
+using RateMe.Core.Enums;
+using RateMe.Persistence.Entities;
+
+namespace RateMe.Persistence.Repositories;
+
+public class UserRepository : IRepository<UserEntity>
+{
+   private readonly RateMeDbContext _context;
+
+   public UserRepository(RateMeDbContext context)
+   {
+      _context = context;
+   }
+
+   public async Task<Guid> Create(UserEntity entity)
+   {
+      var userEntity = new UserEntity
+      {
+         Id = entity.Id,
+         Name = entity.Name,
+         Surname = entity.Surname,
+         About = entity.About,
+         UserName = entity.UserName,
+         HashPassword = entity.HashPassword,
+         Sex = entity.Sex,
+         AvatarLink = entity.AvatarLink,
+         Role = entity.Role,
+      };
+
+      await _context.AddAsync(userEntity);
+      await _context.SaveChangesAsync();
+
+      return userEntity.Id;
+      
+   }
+
+   public async Task<List<UserEntity?>> Get()
+   {
+      var users = await _context.Users.AsNoTracking().ToListAsync();
+
+      return users;
+   }
+   
+
+   public async Task<Guid> Delete(Guid id)
+   {
+       await _context.Users
+         .Where(u => u.Id ==  id)
+         .ExecuteDeleteAsync();
+
+       return id;
+   }
+
+   public async Task<UserEntity?> GetById(Guid id)
+   {
+      return await _context.Users
+         .AsNoTracking()
+         .FirstOrDefaultAsync(u => u.Id == id);
+   }
+}
