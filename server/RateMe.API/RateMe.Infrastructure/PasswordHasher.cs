12ec28a0516d87c4b753cb6638170a362253b026
Artem <118220003+guntersteam@users.noreply.github.com>
2024-09-09 21:49:37 +0300

    feat: added infrastucture layer

---
+++ server/RateMe.API/RateMe.Infrastructure/PasswordHasher.cs
@@ -0,0 +1,13 @@
+﻿using BCrypt.Net;
+
+namespace RateMe.Infrastructure;
+
+public class PasswordHasher
+{
+   public string Generate(string password) =>
+      BCrypt.Net.BCrypt.EnhancedHashPassword(password, HashType.SHA256);
+
+   public bool Verify(string password, string hashPassword) =>
+      BCrypt.Net.BCrypt.EnhancedVerify(password, hashPassword, HashType.SHA256);
+
+}
